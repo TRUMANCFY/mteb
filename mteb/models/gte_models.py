d00660bbae42b56d4d4e5821ef5af61e7e10f353
TRUMANCFY <caifengyutruman@gmail.com>
2024-10-03 13:05:57 +0200

    add 1.5b model for gte-qwen

--- mteb/models/gte_models.py
+++ mteb/models/gte_models.py
@@ -61,6 +61,26 @@ gte_Qwen2_7B_instruct = ModelMeta(
     release_date="2024-06-15",  # initial commit of hf model.
 )
 
+gte_Qwen2_1_5B_instruct = ModelMeta(
+    loader=partial(
+        gte_loader,
+        model_name_or_path="Alibaba-NLP/gte-Qwen2-1.5B-instruct",
+        attn="cccc",
+        pooling_method="lasttoken",
+        mode="embedding",
+        torch_dtype="auto",
+        # The ST script does not normalize while the HF one does so unclear what to do
+        # https://huggingface.co/Alibaba-NLP/gte-Qwen2-7B-instruct#sentence-transformers
+        normalized=True,
+    ),
+    name="Alibaba-NLP/gte-Qwen2-1.5B-instruct",
+    languages=None,
+    open_source=True,
+    revision="e26182b2122f4435e8b3ebecbf363990f409b45b",
+    release_date="2024-06-15",  # initial commit of hf model.
+)
+
+
 
 if __name__ == "__main__":
     # Verify it reproduces https://huggingface.co/Alibaba-NLP/gte-Qwen2-7B-instruct#sentence-transformers
